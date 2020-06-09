e5827adf73dffe7be34f121686d73ebd33f8d804
matheus-reyes <matheus-reyes@usp.br>
2020-06-08 22:13:08 -0300

    Estrutura FxBall

---
+++ EP2/pong2/FxBall.java
@@ -0,0 +1,29 @@
+public class FxBall extends Ball implements IBall{
+
+    /**
+		Construtor da classe FxBall. Recebe o mesmo conjunto de parâmetros que o construtor da superclasse.
+
+		@param cx coordenada x da posição inicial da bola (centro do retangulo que a representa).
+		@param cy coordenada y da posição inicial da bola (centro do retangulo que a representa).
+		@param width largura do retangulo que representa a bola.
+		@param height altura do retangulo que representa a bola.
+		@param color cor da bola.
+		@param speed velocidade da bola (em pixels por millisegundo).
+		@param vx componente x do vetor unitário (normalizado) que representa a direção da bola.
+		@param vy componente y do vetor unitário (normalizado) que representa a direção da bola.
+	*/
+
+    public FxBall(double cx, double cy, double width, double height, java.awt.Color color, double speed, double vx, double vy){
+        super(cx, cy, width, height, color, speed, vx, vy);
+    }
+
+    /**
+		Método que redefine o comportamento de desenho definido na superclasse.
+	*/
+
+	public void draw(){
+
+
+		
+	}
+}
