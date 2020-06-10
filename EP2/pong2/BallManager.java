68c9b8ad7dcf1ec9849c4debc04fa39bb1db8200
matheus-reyes <matheus-reyes@usp.br>
2020-06-10 19:19:15 -0300

    Efeito Rastro Funcional

--- EP2/pong2/BallManager.java
+++ EP2/pong2/BallManager.java
@@ -136,6 +136,7 @@ public class BallManager {
 		while (iteratorBalls.hasNext()){
 			iteratorBalls.next().draw();
 		}
+
 	}
 	
 	/**
--- EP2/pong2/FxBall.java
+++ EP2/pong2/FxBall.java
@@ -1,4 +1,25 @@
+import java.util.*;
+import java.awt.Color;
+
 public class FxBall extends Ball implements IBall{
+
+	/**
+		Atributo privado que representa uma fila de coordenadas x em que a bola passou
+	*/
+
+	private Queue <Double> ballCx = new LinkedList <Double> ();
+
+	/**
+		Atributo privado que representa uma fila de coordenadas y em que a bola passou
+	*/
+
+	private Queue <Double> ballCy = new LinkedList <Double> ();
+
+	/**
+		Atributo privado que representa o início do jogo
+	*/
+
+	private boolean inicio = false;
 
     /**
 		Construtor da classe FxBall. Recebe o mesmo conjunto de parâmetros que o construtor da superclasse.
@@ -22,6 +43,45 @@ public class FxBall extends Ball implements IBall{
 	*/
 
 	public void draw(){
+
 		super.draw();
+
+		//Se é a primeira vez que o método draw é chamado, pega as 20 primeiras posições anteriores da bola
+		if(inicio == false){
+			for(int i = 0; i < 20; i++){
+				ballCx.add(super.getCx());
+				ballCy.add(super.getCy());
+			}
+		}
+
+		//Depois define a variável inicio como true para o procedimento anterior não ocorrer novamente
+		inicio = true;
+
+		//Elimina as duas últimas posições
+		ballCx.poll();
+		ballCy.poll();
+		//Adiciona as duas últimas posições
+		ballCx.add(super.getCx());
+		ballCy.add(super.getCy());
+
+		//Iterador da lista de coordenadas x
+		Iterator <Double> iteratorCx = ballCx.iterator();
+
+		//Iterador da lista de coordenadas y
+		Iterator <Double> iteratorCy = ballCy.iterator();
+		
+		//Variável que irá alterar a altura do efeito de rastro
+		double refinador = 0.0;
+
+		//While que percorre as duas filas
+		while (iteratorCx.hasNext() && iteratorCy.hasNext()) {
+
+			//Desenha um retângulo para representar o efeito de rastro passando as coordenadas antigas
+			GameLib.fillRect(iteratorCx.next(), iteratorCy.next(), super.getWidth() / 2, (super.getHeight() / 4) + refinador);
+			
+			//Atualiza o refinador para melhorar a aparência do efeito de rastro
+			refinador += 0.8;
+		}
+		
 	}
 }
